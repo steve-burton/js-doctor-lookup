9097328d1b781230963608ec7520ebb4698c0437
Steve Burton <stevevburton@gmail.com>
2017-01-06 15:02:40 -0800

    add for loop and push name to array

--- js/doctor.js
+++ js/doctor.js
@@ -1,24 +1,34 @@
 var apiKey = require('./../.env').apiKey;
 
 Doctor = function(medicalIssue) {
-  this.doctorList = [];
   this.doctorMedicalIssue = medicalIssue;
 };
 // console.log(medicalIssue);
 Doctor.prototype.getDoctors = function(medicalIssue) {
   var newDoctor = this;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=Toothache&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-    var searchDoctors = (response.data[0].practices[0].name);
+    // var searchDoctors = (response.data[0].practices[0]);
       console.log(response);
-      console.log(searchDoctors);
+      // var searchDoctors = (response.data);
+      // var doctorList = [];
+      // console.log(searchDoctors);
+      // for(var index = 1; index <= 20; index ++) {
+      //   doctorList.push(searchDoctors.profile.first_name);
+      //   console.log(searchDoctors.first_name);
+      //   console.log("doctorList " + doctorList);
+      // }
+
+      var searchDoctors = (response.data);
+      var doctorList = [];
       searchDoctors.forEach(function(doctor) {
-        this.doctorList.push(doctor);
-        console.log(doctor);
-      })
-      .fail(function(error){
+        doctorList.push(doctor.profile.first_name, doctor.profile.last_name);
+        // console.log(doctor.profile.first_name);
+      });
+      console.log(doctorList);
+    })
+    .fail(function(error){
       console.log("fail");
-      $("h5.response").text("newDoctor.doctorList " + newDoctor.doctorList);
-    });
+      $("h5.response").append("doctorList " + doctorList);
   });
 };
 
