8ec6683fc386bcc2034cae801d83c94525513160
Steve-Burton <stevevburton@gmail.com>
2017-01-07 14:28:11 -0800

    add functioning to output practice and phone with doctor

--- js/doctor.js
+++ js/doctor.js
@@ -7,17 +7,18 @@ Doctor = function(medicalIssue) {
 Doctor.prototype.getDoctors = function(medicalIssue) {
   var newDoctor = this;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+    console.log(response);
       var searchDoctors = (response.data);
       var doctorList = [];
       searchDoctors.forEach(function(doctor) {
-        doctorList.push(" " + doctor.profile.first_name + " " + doctor.profile.last_name + "<br>");
+        doctorList.push("<h5>" + doctor.profile.first_name + " " + doctor.profile.last_name + " - " + "<span class='practice'>" + "Practice name: " + doctor.practices[0].name + ", Phone: " + doctor.practices[0].phones[0].number.slice(0, 3) + "-" + doctor.practices[0].phones[0].number.slice(3, 6) + "-" + doctor.practices[0].phones[0].number.slice(6, 10) + " " + doctor.practices[0].phones[0].type + "</span></h5>");
       });
-      $('h5.response').hide();
-      $('h5.response').show();
+      $('div.response').hide();
+      $('div.response').show();
       $('.result').show();
-      $('h5.response').html(doctorList);
+      $('div.response').html(doctorList);
     }).fail(function(error){
-      $('h5.response').text(error.responseJSON.message);
+      $('div.response').text(error.responseJSON.message);
   });
 };
 
