05a52c3f4e755ed5d299e21823c887f585210b80
Steve-Burton <stevevburton@gmail.com>
2017-01-07 14:52:57 -0800

    remove console log from js file and done

--- js/doctor.js
+++ js/doctor.js
@@ -7,7 +7,6 @@ Doctor = function(medicalIssue) {
 Doctor.prototype.getDoctors = function(medicalIssue) {
   var newDoctor = this;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-    console.log(response);
       var searchDoctors = (response.data);
       var doctorList = [];
       searchDoctors.forEach(function(doctor) {
